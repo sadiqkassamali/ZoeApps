3f01d12aeea0713c61c39dec3fe3c88a67927d04
Sadiq Kassamali <sadiqkassamali@gmail.com>
2025-02-24 00:15:19 -0600

    Another typo missplaced
    /s

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { useState } from "react";
-import "../styles/globals.css";
+import "../styles/global.css";
 
 const categories = {
   "Automated Testing": [
