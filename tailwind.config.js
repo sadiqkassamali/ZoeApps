9c38dcc55a8e303c3b1977b595138a76932f55db
Sadiq Kassamali <sadiqkassamali@gmail.com>
2025-02-24 00:04:00 -0600

    Fixing tail wind CSS
    module.exports = {
      content: [
        "./pages/**/*.{js,ts,jsx,tsx}",
        "./components/**/*.{js,ts,jsx,tsx}",
      ],
      darkMode: "class",
      theme: {
        extend: {},
      },
      plugins: [],
    };

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // Enables dark mode toggle
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+  plugins: [],
 };
