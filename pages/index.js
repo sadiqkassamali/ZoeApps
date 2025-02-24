1e4ae863fce63ccce9fe795ab66d79840d9eca89
Sadiq Kassamali <sadiqkassamali@gmail.com>
2025-02-24 00:31:05 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { useState } from "react";
+import "../styles/global.css";
 
 const categories = {
   "Automated Testing": [
