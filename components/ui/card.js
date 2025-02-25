3bbdc15c339d111fe26c66357b567ea202b587b2
Sadiq Kassamali <sadiqkassamali@gmail.com>
2025-02-25 01:51:55 -0600

    Update and rename card.js to card

--- components/ui/card.js
+++
@@ -1,7 +0,0 @@
-export function Card({ children, className = "" }) {
-  return (
-    <div className={`p-6 shadow-lg rounded-lg bg-white dark:bg-gray-800 ${className}`}>
-      {children}
-    </div>
-  );
-}
