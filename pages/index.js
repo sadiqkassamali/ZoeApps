83a7049578df5c509bc83c428be46075a69d77dd
Sadiq Kassamali <sadiqkassamali@gmail.com>
2025-02-25 01:47:20 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Head from "next/head";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "../components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
 import { motion } from "framer-motion";
 import { signIn } from "next-auth/react";
 
