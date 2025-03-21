f3181ea51d0c2dc7dc4dce96ae93839f66955be7
Sadiq Kassamali <sadiqkassamali@gmail.com>
2025-03-21 00:01:23 -0500

    Update request-demo.js

--- pages/request-demo.js
+++ pages/request-demo.js
@@ -53,13 +53,13 @@ export default function RequestDemo() {
   ];
 
   const timeSlots = [
-    "9:00 AM - 10:00 AM",
-    "10:00 AM - 11:00 AM",
-    "11:00 AM - 12:00 PM",
-    "1:00 PM - 2:00 PM",
-    "2:00 PM - 3:00 PM",
-    "3:00 PM - 4:00 PM",
-    "4:00 PM - 5:00 PM",
+    '9:00 AM - 10:00 AM',
+    '0:00 AM - 11:00 AM",
+    '1:00 AM - 12:00 PM",
+    '1:00 PM - 2:00 PM",
+    '2:00 PM - 3:00 PM",
+    '3:00 PM - 4:00 PM",
+    '4:00 PM - 5:00 PM",
   ];
 
   const handleDemoSubmit = async (e: React.FormEvent) => {
