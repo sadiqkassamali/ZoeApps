7f2edbe223240d2351bb594dcd43f50d36d787aa
Sadiq Kassamali <sadiqkassamali@gmail.com>
2025-03-22 15:54:33 -0500

    Update textarea.js

--- components/ui/textarea.js
+++ components/ui/textarea.js
@@ -1,3 +1,6 @@
-export const Textarea = (props) => (
-  <textarea className="w-full border rounded px-3 py-2" {...props} />
-)
+export const Textarea = ({ className = "", ...props }) => (
+  <textarea
+    className={`w-full border rounded px-3 py-2 bg-white dark:bg-gray-800 dark:text-white resize-y ${className}`}
+    {...props}
+  />
+);
