0c691f006c91ea83a5b861f4d08db29f9b0f2d17
Sadiq Kassamali <sadiqkassamali@gmail.com>
2025-03-22 15:53:38 -0500

    Update label.js

--- components/ui/label.js
+++ components/ui/label.js
@@ -1,5 +1,8 @@
-export const Label = ({ children, htmlFor }) => (
-  <label htmlFor={htmlFor} className="block text-sm font-medium text-gray-700 mb-1">
+export const Label = ({ children, htmlFor, className = "" }) => (
+  <label
+    htmlFor={htmlFor}
+    className={`block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1 ${className}`}
+  >
     {children}
   </label>
-)
+);
