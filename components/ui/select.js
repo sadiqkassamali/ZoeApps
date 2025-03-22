155bcb1235172366f812e5062407dfd9fa9763c2
Sadiq Kassamali <sadiqkassamali@gmail.com>
2025-03-22 15:54:06 -0500

    Update select.js

--- components/ui/select.js
+++ components/ui/select.js
@@ -1,8 +1,8 @@
-export const Select = ({ options = [], value, onChange }) => (
+export const Select = ({ options = [], value, onChange, className = "" }) => (
   <select
-    className="w-full border rounded px-3 py-2"
     value={value}
     onChange={onChange}
+    className={`w-full border rounded px-3 py-2 bg-white dark:bg-gray-800 dark:text-white ${className}`}
   >
     {options.map((opt) => (
       <option key={opt.value} value={opt.value}>
@@ -10,4 +10,4 @@ export const Select = ({ options = [], value, onChange }) => (
       </option>
     ))}
   </select>
-)
+);
