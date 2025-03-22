5a8600c05f2f115fc262836f0ef2191165f0b82c
Sadiq Kassamali <sadiqkassamali@gmail.com>
2025-03-22 15:53:15 -0500

    Update input.js

--- components/ui/input.js
+++ components/ui/input.js
@@ -1,3 +1,11 @@
-export const Input = ({ type = 'text', ...props }) => (
-  <input type={type} className="w-full border rounded px-3 py-2" {...props} />
-)
+export const Input = ({
+  type = "text",
+  className = "",
+  ...props
+}) => (
+  <input
+    type={type}
+    className={`w-full border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 text-gray-900 dark:text-white rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+    {...props}
+  />
+);
